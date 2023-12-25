e566fd31ade32ad0051e5e239f8e5d6f933da416
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-26 00:11:22 +0800

    created jswebtoken generation and response as a cookie

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,56 @@
-const { sendResponse } = require("../utils/helper");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { sendResponse, generateToken } = require("../utils/helper");
 
-const registerUser = (req, res) => {
-  sendResponse(res, "message", "Register user", 201);
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  // Validation
+  if (!name || !email || !password) {
+    return sendResponse(res, "error", "Please fill in all the fields.", 400);
+  }
+
+  if (password.length < 8) {
+    return sendResponse(
+      res,
+      "error",
+      "Password must be at least 8 characters long.",
+      400
+    );
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return sendResponse(res, "error", "Email already used.", 409);
+  }
+
+  // Create new user
+  const user = await User.create({ name, email, password });
+
+  // Generate token
+  const token = generateToken(user._id);
+
+  // Send HTTP-only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400),
+    sameSite: "none",
+    secure: true,
+  });
+
+  if (user) {
+    const { _id, name, email, phone, bio, photo, role, isVerified } = user;
+    sendResponse(
+      res,
+      "user",
+      { _id, name, email, phone, bio, photo, role, isVerified, token },
+      201
+    );
+  } else {
+    sendResponse(res, "error", "Invalid user data", 400);
+  }
+});
 
 module.exports = { registerUser };
--- utils/helper.js
+++ utils/helper.js
@@ -1,7 +1,13 @@
+const jwt = require("jsonwebtoken");
+
 exports.sendResponse = (res, type, message, statusCode = 401) => {
   res.status(statusCode).json({ [type]: message });
 };
 
 exports.handleNotFound = (req, res) => {
   this.sendResponse(res, "error", "This page doesn't exist.", 404);
+};
+
+exports.generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
