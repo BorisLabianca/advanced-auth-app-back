b00289c4692162ddea6a4a6c17029089175ed43a
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-31 18:37:22 +0800

    added origin and credentials to cors

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://authz-app.vercel.app"],
+    credentials: true,
+  })
+);
 
 app.use("/api/users", userRoutes);
 
